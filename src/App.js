5018a9e0a7e85e0e26d2120a34a128e70c1a2c73
bacem555 <146827018+bacem555@users.noreply.github.com>
2023-12-11 23:28:09 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {moviesData} from './data'
+import './App.css';
+import { useState } from 'react';
+import Movielist from './components/Movielist';
+import Addnewmovie from './components/Addnewmovie';
+
+function App() {
+  const [data, setData] = useState(moviesData)
+  const HandleDel=(ID)=>setData(data.filter(el=>el.id!==ID))
+  const handleadd=(newmovie)=>setData([...data,newmovie])
+  const handleEdit=(editedmovie)=>setData(data.map(el=>el.id===editedmovie.id?editedmovie:el))
+  // console.log(moviesData)
+
+  return (
+    <div className="App">
+       <Addnewmovie  handleadd={ handleadd}/>
+      <Movielist list={data}  HandleDel={HandleDel} handleEdit={handleEdit} />
+     
+     
+    </div>
+  );
+}
+
+export default App;
